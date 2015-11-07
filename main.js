5334cabfd8fafbf5ae34159176e776c537b20319
Samuel Robertson <samuel.robertson93@gmail.com>
2015-11-06 23:16:55 -0800

    setup ajax structure

--- main.js
+++ main.js
@@ -2,10 +2,38 @@
 
 $(document).ready(function() {
 
-  $('#a-button').click(doThing);
+  $('#a-button').click(getFaceData);
 
-  function doThing() {
-    console.log('a thing');
+  function getFaceData() {
+    var imageParams = {
+           // Request parameters
+           "analyzesFaceLandmarks": "false",
+           "analyzesAge": "true",
+           "analyzesGender": "true",
+           "analyzesHeadPose": "false",
+       };
+
+       $.ajax({
+         url: 'https://api.projectoxford.ai/face/v0/detections&' + $.param(imageParams),
+         beforeSend:
+         type: 'default GET (Other values: POST)',
+         dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+         data: {param1: 'value1'}
+       })
+       .done(function() {
+         console.log("success");
+       })
+       .fail(function() {
+         console.log("error");
+       })
+       .always(function() {
+         console.log("complete");
+       });
+
+
   }
+
+
+
 
 });
